75737f88cdd59bb327e226b366f611303a10ed96
Lee Stratman <stratmanlc10@gmail.com>
2021-02-02 18:15:14 -0600

    Add UserInterface class to Game.js. Add necessary functions to Game.js.

--- Game.js
+++ Game.js
@@ -1,24 +1,41 @@
 "use strict";
 
-const prompt = require("prompt-sync")();
+const { UI } = require("./UserInterface");
 const { Player } = require("./Player");
 const { AI } = require("./AI");
 class Game {
   constructor() {
-    this.gameType = prompt("Game mode (single player or multiplayer)?");
+    this.UI = new UserInterface();
+    this.setupGame();
+    this.createPlayers();
+  }
 
-    this.playerOne = new Player(prompt("Enter name for player 1: "));
+  setupGame() {
+    this.isSingle =
+      this.UI.choose(["Single Player", "Multiplayer"]) === "1" ? true : false;
+    this.rounds = 5;
+  }
 
-    if (this.gameType == "single") {
-      this.playerTwo = new Player(prompt("Enter name for player 2: "));
-    } else {
-      this.playerTwo = new AI();
-    }
+  createPlayers() {
+    this.players = [];
+
+    this.players.push(new Player(this.UI.ask("Enter name for player 1: ")));
+    this.isSingle
+      ? this.players.push(new AI())
+      : this.players.push(new Player(this.UI.ask("Enter name for player 2: ")));
   }
 
   startGame() {
     console.log("Game Start");
   }
+
+  displayRules() {}
+
+  endGame() {}
+
+  calculateWinner() {}
+
+  isOver() {}
 }
 
 module.exports.Game = Game;
